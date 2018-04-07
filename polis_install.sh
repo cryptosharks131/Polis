4629e7a0710823586ad4df5087daa1da35f4478e
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-04-07 11:53:31 +0100

    Update polis_install.sh

--- polis_install.sh
+++ polis_install.sh
@@ -129,6 +129,7 @@ clear
 }
 
 function update_config() {
+  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 logintimestamps=1
 maxconnections=256
