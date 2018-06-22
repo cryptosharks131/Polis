f80669cf3bcd049d41755757e533d7eae6ffc0af
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2018-06-21 20:49:14 -0500

    Update aced_install.sh

--- aced_install.sh
+++ aced_install.sh
@@ -9,7 +9,7 @@ COIN_REPO='https://github.com/Acedcoin/AceD/releases/download/1.5/ubuntu16mn.tar
 #SENTINEL_REPO='https://github.com/cryptosharks131/sentinel'
 COIN_NAME='AceD'
 COIN_PORT=24126
-#COIN_BS='http://149.28.231.161/bootstrap.zip'
+#COIN_BS='http://bootstrap.zip'
 
 
 NODEIP=$(curl -s4 icanhazip.com)
