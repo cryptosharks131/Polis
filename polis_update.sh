f2e278c040be6e55552b2cf58ff6c7cb06313a6e
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2018-07-16 23:19:21 -0500

    Update polis_update.sh

--- polis_update.sh
+++ polis_update.sh
@@ -28,7 +28,7 @@ function update_node() {
   COIN_ZIP=$(echo $COIN_REPO | awk -F'/' '{print $NF}')
   tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
   compile_error
-  cp poliscore-1.3.1/bin/polis{d,-cli} /usr/local/bin
+  cp bin/polis{d,-cli} /usr/local/bin
   compile_error
   strip $COIN_DAEMON $COIN_CLI
   cd - >/dev/null 2>&1
