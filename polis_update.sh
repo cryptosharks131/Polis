4914494b6b9a002f7eb14b663ebe4aab969a571e
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-01-30 18:36:02 -0600

    Update polis_update.sh

--- polis_update.sh
+++ polis_update.sh
@@ -9,7 +9,7 @@ COIN_CLI='/usr/local/bin/polis-cli'
 COIN_REPO='https://github.com/polispay/polis/releases/download/v1.4.8.1/poliscore-1.4.8.1-x86_64-linux-gnu.tar.gz'
 SENTINEL_REPO='https://github.com/polispay/sentinel.git'
 COIN_NAME='Polis'
-COIN_BS='https://github.com/cryptosharks131/Polis/releases/download/v1.4.8.1/bootstrap.tar.gz'
+COIN_BS='http://wbs.cryptosharkspool.com/polis/bootstrap.tar.gz'
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
