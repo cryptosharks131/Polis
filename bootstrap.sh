6e0d48ac7e6dc964b1f2bb044a302a98070c45e2
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-02-03 14:25:26 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,21 +3,23 @@
   COIN_DAEMON='/usr/local/bin/polisd'
   COIN_CLI='/usr/local/bin/polis-cli'
   COIN_NAME='Polis'
-  COIN_BS='http://wbs.cryptosharkspool.com/polis/bootstrap.tar.gz'  
+  COIN_BS='http://explorer.polispay.org/images/bootstrap.dat'  
 
   echo -e "Importing Bootstrap For $COIN_NAME"
   systemctl stop $COIN_NAME >/dev/null 2>&1
   $COIN_CLI stop >/dev/null 2>&1
   sleep 3
   pkill -9 $COIN_DAEMON
-  rm -r $CONFIGFOLDER/blocks $CONFIGFOLDER/chainstate $CONFIGFOLDER/peers.dat $CONFIGFOLDER/banlist.dat
-  cd $TMP_BS
+  cd $CONFIGFOLDER
+#   rm -r $CONFIGFOLDER/blocks $CONFIGFOLDER/chainstate $CONFIGFOLDER/peers.dat $CONFIGFOLDER/banlist.dat
+#   cd $TMP_BS
   wget -q $COIN_BS
-  COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
-  tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
-  cp -r blocks chainstate peers.dat $CONFIGFOLDER
-  cd - >/dev/null 2>&1
-  rm -rf $TMP_BS >/dev/null 2>&1
+  cd
+#   COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
+#   tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
+#   cp -r blocks chainstate peers.dat $CONFIGFOLDER
+#   cd - >/dev/null 2>&1
+#   rm -rf $TMP_BS >/dev/null 2>&1
   #$COIN_DAEMON -daemon -reindex
   #sleep 15
   #$COIN_CLI stop >/dev/null 2>&1
