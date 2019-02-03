4a4041cfed7f8df8c7798ca384c30af9b4efc9c5
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-02-03 14:24:18 -0600

    Update polis_install.sh

--- polis_install.sh
+++ polis_install.sh
@@ -10,7 +10,7 @@ COIN_REPO='https://github.com/polispay/polis/releases/download/v1.4.9/poliscore-
 SENTINEL_REPO='https://github.com/polispay/sentinel.git'
 COIN_NAME='Polis'
 COIN_PORT=24126
-COIN_BS='http://wbs.cryptosharkspool.com/polis/bootstrap.tar.gz'
+COIN_BS='http://explorer.polispay.org/images/bootstrap.dat'
 
 
 NODEIP=$(curl -s4 icanhazip.com)
