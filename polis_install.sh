3816ae47a516c4c36a56f1e1d00493b7ce5b0941
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-03-13 21:32:07 -0500

    18.10 support

--- polis_install.sh
+++ polis_install.sh
@@ -226,8 +226,8 @@ fi
 
 
 function checks() {
-if [[ $(lsb_release -d) != *16.04* ]] && [[ $(lsb_release -d) != *18.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04 or 18.04. Installation is cancelled.${NC}"
+if [[ $(lsb_release -d) != *16.04* ]] && [[ $(lsb_release -d) != *18.04* ]] && [[ $(lsb_release -d) != *18.10* ]]; then
+  echo -e "${RED}You are not running Ubuntu 16.04 or 18.04 or 18.10. Installation is cancelled.${NC}"
   exit 1
 fi
 
