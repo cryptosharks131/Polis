1fe7c12ea9ed9389b1f6b1e80ada1a550060facd
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-04-28 13:14:52 -0500

    Lower max connections

--- polis_install.sh
+++ polis_install.sh
@@ -142,7 +142,7 @@ function update_config() {
   sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 logintimestamps=1
-maxconnections=256
+maxconnections=64
 #bind=$NODEIP
 masternode=1
 externalip=$NODEIP:$COIN_PORT
