4e2db13440e126fba606fa7f050e8ab823a9bcd2
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-05-09 18:30:39 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@
 #   cd
   COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
   tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
-  cp -r blocks chainstate peers.dat $CONFIGFOLDER
+  cp -r blocks chainstate $CONFIGFOLDER
   cd - >/dev/null 2>&1
   rm -rf $TMP_BS >/dev/null 2>&1
   #$COIN_DAEMON -daemon -reindex
