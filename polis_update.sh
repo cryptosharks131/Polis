563ea732ce1fe56b6be496715052c2518f95a02e
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-05-12 16:42:14 -0500

    reindex on update, resetting peers

--- polis_update.sh
+++ polis_update.sh
@@ -91,17 +91,17 @@ clear
 function import_bootstrap() {
   echo -e "Importing Bootstrap For $COIN_NAME"
   rm -rf $CONFIGFOLDER/blocks $CONFIGFOLDER/chainstate $CONFIGFOLDER/peers.dat $CONFIGFOLDER/banlist.dat $CONFIGFOLDER/mncache.dat
-#   cd $TMP_BS
+  cd $TMP_BS
 #   cd $CONFIGFOLDER
-#   wget -q $COIN_BS
-#   compile_error
+  wget -q $COIN_BS
+  compile_error
 #   cd
-#   COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
-#   tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
-#   compile_error
-#   cp -r blocks chainstate $CONFIGFOLDER
-#   cd - >/dev/null 2>&1
-#   rm -rf $TMP_BS >/dev/null 2>&1
+  COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
+  tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
+  compile_error
+  cp -r blocks chainstate $CONFIGFOLDER
+  cd - >/dev/null 2>&1
+  rm -rf $TMP_BS >/dev/null 2>&1
   clear
 }
 
@@ -147,10 +147,11 @@ EOF
 }
 
 function important_information() {
-#  $COIN_DAEMON -daemon -reindex
-#  sleep 15
-#  $COIN_CLI stop >/dev/null 2>&1
-#  sleep 5
+ rm $CONFIGFOLDER/{peers.dat,banlist.dat}
+ $COIN_DAEMON -daemon -reindex
+ sleep 15
+ $COIN_CLI stop >/dev/null 2>&1
+ sleep 5
  systemctl start $COIN_NAME >/dev/null 2>&1
  sleep 3
  $COIN_DAEMON -daemon >/dev/null 2>&1
@@ -170,7 +171,7 @@ clear
 checks
 prepare_system
 update_node
-import_bootstrap
+#import_bootstrap
 update_config
 update_sentinel
 important_information
