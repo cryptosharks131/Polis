b376bd06b3081adc4788c388a8f8f67f363acbac
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-06-18 17:00:00 -0500

    v1.4.16

--- polis_update.sh
+++ polis_update.sh
@@ -6,7 +6,7 @@ CONFIGFOLDER='/root/.poliscore'
 CONFIG_FILE='polis.conf'
 COIN_DAEMON='/usr/local/bin/polisd'
 COIN_CLI='/usr/local/bin/polis-cli'
-COIN_REPO='https://github.com/polispay/polis/releases/download/v1.4.15/poliscore-1.4.15-x86_64-linux-gnu.tar.gz'
+COIN_REPO='https://github.com/polispay/polis/releases/download/v1.4.16a/poliscore-1.4.16-x86_64-linux-gnu.tar.gz'
 SENTINEL_REPO='https://github.com/polispay/sentinel.git'
 COIN_NAME='Polis'
 COIN_BS='https://github.com/polispay/polis/releases/download/v1.4.15/bootstrap.tar.gz'
@@ -114,10 +114,10 @@ function update_config() {
 
 function important_information() {
 #  rm -rf $CONFIGFOLDER/blocks $CONFIGFOLDER/chainstate $CONFIGFOLDER/peers.dat $CONFIGFOLDER/banlist.dat $CONFIGFOLDER/mncache.dat
- $COIN_DAEMON -daemon -reindex
- sleep 15
- $COIN_CLI stop >/dev/null 2>&1
- sleep 5
+#  $COIN_DAEMON -daemon -reindex
+#  sleep 15
+#  $COIN_CLI stop >/dev/null 2>&1
+#  sleep 5
  systemctl start $COIN_NAME >/dev/null 2>&1
  sleep 3
  $COIN_DAEMON -daemon >/dev/null 2>&1
