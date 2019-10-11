ae8c148a7260d20a49e3479918da84aaf0bb2c03
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2019-10-11 18:10:26 -0500

    remove reindex

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,13 +17,13 @@
 #   cd
   COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
   tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
-  cp -r blocks chainstate $CONFIGFOLDER
+  cp -r blocks chainstate peers.dat $CONFIGFOLDER
   cd - >/dev/null 2>&1
   rm -rf $TMP_BS >/dev/null 2>&1
-  $COIN_DAEMON -daemon -reindex
-  sleep 15
-  $COIN_CLI stop >/dev/null 2>&1
-  sleep 5
+#   $COIN_DAEMON -daemon -reindex
+#   sleep 15
+#   $COIN_CLI stop >/dev/null 2>&1
+#   sleep 5
   systemctl start $COIN_NAME >/dev/null 2>&1
   sleep 3
   $COIN_DAEMON -daemon >/dev/null 2>&1
