01cb4919d8700b071263fc5f120602cf0f487739
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2020-04-10 09:08:20 -0500

    remove masternodeprivkey

--- polis_install.sh
+++ polis_install.sh
@@ -113,58 +113,33 @@ EOF
 }
 
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
-  read -e COINKEYOLD
-  if [[ -z "$COINKEYOLD" ]];
-  then
-    $COIN_DAEMON -daemon
-    sleep 30
-    if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-     echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
-     exit 1
-    fi
-    COINKEYOLD=$($COIN_CLI masternode genkey)
-    COINKEY=$($COIN_CLI bls generate)
-    COINKEYPRIVRAW=$(echo "$COINKEY" | grep -Po '"secret": ".*?[^\\]"' | cut -c12-)
-    COINKEYPRIV=${COINKEYPRIVRAW::-1}
-    COINKEYPUBRAW=$(echo "$COINKEY" | grep -Po '"public": ".*?[^\\]"' | cut -c12-)
-    COINKEYPUB=${COINKEYPUBRAW::-1}
-    if [ "$?" -gt "0" ];
-      then
-      echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-      sleep 30
-      COINKEYOLD=$($COIN_CLI masternode genkey)
-      COINKEY=$($COIN_CLI bls generate)
-      COINKEYPRIVRAW=$(echo "$COINKEY" | grep -Po '"secret": ".*?[^\\]"' | cut -c12-)
-      COINKEYPRIV=${COINKEYPRIVRAW::-1}
-      COINKEYPUBRAW=$(echo "$COINKEY" | grep -Po '"public": ".*?[^\\]"' | cut -c12-)
-      COINKEYPUB=${COINKEYPUBRAW::-1}
-    fi
-    $COIN_CLI stop
-  else
-    $COIN_DAEMON -daemon
-    sleep 30
-    if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-     echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
-     exit 1
-    fi
-    COINKEY=$($COIN_CLI bls generate)
-    COINKEYPRIVRAW=$(echo "$COINKEY" | grep -Po '"secret": ".*?[^\\]"' | cut -c12-)
-    COINKEYPRIV=${COINKEYPRIVRAW::-1}
-    COINKEYPUBRAW=$(echo "$COINKEY" | grep -Po '"public": ".*?[^\\]"' | cut -c12-)
-    COINKEYPUB=${COINKEYPUBRAW::-1}
-    if [ "$?" -gt "0" ];
-      then
-      echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-      sleep 30
-      COINKEY=$($COIN_CLI bls generate)
-      COINKEYPRIVRAW=$(echo "$COINKEY" | grep -Po '"secret": ".*?[^\\]"' | cut -c12-)
-      COINKEYPRIV=${COINKEYPRIVRAW::-1}
-      COINKEYPUBRAW=$(echo "$COINKEY" | grep -Po '"public": ".*?[^\\]"' | cut -c12-)
-      COINKEYPUB=${COINKEYPUBRAW::-1}
-    fi
-    $COIN_CLI stop
+  echo -e "Generating a new BLS ${RED}Masternode Private Key${NC} for you:"
+#   read -e COINKEY
+#   echo -e "Generating a new ${RED}BLS Private Key${NC}"
+#   if [[ -z "$COINKEY" ]]; then
+  $COIN_DAEMON -daemon
+  sleep 30
+  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
+   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
+   exit 1
   fi
+  COINKEY=$($COIN_CLI bls generate)
+  COINKEYPRIVRAW=$(echo "$COINKEY" | grep -Po '"secret": ".*?[^\\]"' | cut -c12-)
+  COINKEYPRIV=${COINKEYPRIVRAW::-1}
+  COINKEYPUBRAW=$(echo "$COINKEY" | grep -Po '"public": ".*?[^\\]"' | cut -c12-)
+  COINKEYPUB=${COINKEYPUBRAW::-1}
+  if [ "$?" -gt "0" ];
+    then
+    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
+    sleep 30
+    COINKEY=$($COIN_CLI bls generate)
+    COINKEYPRIVRAW=$(echo "$COINKEY" | grep -Po '"secret": ".*?[^\\]"' | cut -c12-)
+    COINKEYPRIV=${COINKEYPRIVRAW::-1}
+    COINKEYPUBRAW=$(echo "$COINKEY" | grep -Po '"public": ".*?[^\\]"' | cut -c12-)
+    COINKEYPUB=${COINKEYPUBRAW::-1}
+  fi
+  $COIN_CLI stop
+# fi
 clear
 }
 
@@ -174,7 +149,6 @@ logintimestamps=1
 maxconnections=64
 masternode=1
 externalip=$NODEIP:$COIN_PORT
-masternodeprivkey=$COINKEYOLD
 masternodeblsprivkey=$COINKEYPRIV
 addnode=insight.polispay.org
 addnode=116.203.116.205
@@ -295,7 +269,6 @@ function important_information() {
  echo -e "Start: ${RED}systemctl start $COIN_NAME.service${NC}"
  echo -e "Stop: ${RED}systemctl stop $COIN_NAME.service${NC}"
  echo -e "VPS_IP:PORT ${RED}$NODEIP:$COIN_PORT${NC}"
- echo -e "MASTERNODE PRIVATEKEY is: ${RED}$COINKEYOLD${NC}"
  echo -e "MASTERNODE BLS PUBKEY is: ${RED}$COINKEYPUB${NC}"
  if [[ -n $SENTINEL_REPO  ]]; then
   echo -e "${RED}Sentinel${NC} is installed in ${RED}/sentinel${NC}"
