c68e9d5b5466cb850b97353bb7121c1e7e7ef465
cryptosharks131 <38626122+cryptosharks131@users.noreply.github.com>
2020-09-20 19:20:24 -0500

    add strip

--- polis_install.sh
+++ polis_install.sh
@@ -285,7 +285,7 @@ function import_bootstrap() {
   wget -q $COIN_BS
   compile_error
   COIN_ZIP=$(echo $COIN_BS | awk -F'/' '{print $NF}')
-  tar xvf $COIN_ZIP >/dev/null 2>&1
+  tar xvf $COIN_ZIP --strip 1 >/dev/null 2>&1
   compile_error
   cp -r evodb blocks chainstate peers.dat $CONFIGFOLDER
   cd - >/dev/null 2>&1
